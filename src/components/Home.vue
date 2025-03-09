36a1533f6aeee3c110d278b7ac55372225a02612
Codechad122 <footballers737@gmail.com>
2025-03-09 13:45:55 -0500

    add basic home features list and dynamic text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from './Navbar.vue'
 import Footer from './Footer.vue'
+import HomeFeaturesList from './HomeFeaturesList.vue'
 
 </script>
 
@@ -8,7 +9,17 @@ import Footer from './Footer.vue'
     <header>
         <Navbar />
     </header>
-    <main class="h-screen">
+    <main class="w-full  mx-auto p-4 flex flex-col">
+        <div class="flex justify-center">
+            <h1 class="text-2xl">
+                The Best Coffee That Money Can Buy
+            </h1>
+        </div>
+        <br/>
+
+        <div class="flex gap-4 justify-center">
+            <HomeFeaturesList/>
+        </div>
 
     </main>
     <Footer />
---
+++ src/components/HomeFeaturesList.vue
@@ -0,0 +1,30 @@
+<script setup>
+const data = [
+    {title: "Discover Our Premium Coffee Selection", info: "Explore our curated collection of the finest coffees, each crafted to deliver a rich and unparalleled flavor experience.", button: "Shop Now"},
+    {title: "Experience Artisanal Coffee Craftsmanship", info: "Delve into the world of artisanal coffee, where every brew is a masterpiece of flavor and quality.", button: "Learn More"},
+    {title: "Elevate Your Coffee Ritual", info: "Transform your daily coffee routine with our exceptional brews, designed for true coffee connoisseurs.", button: "Browse Collection"}
+]
+
+</script>
+
+<template>
+    <div v-for="n in data" :key="n">
+        <div class="card bg-base-100 w-96 shadow-sm h-96">
+            <figure class="px-10 pt-10">
+                <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+                class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">{{ n.title }}</h2>
+                <p>{{ n.info }}</p>
+                <div class="card-actions">
+                <button class="btn btn-primary">{{ n.button }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+</template>
