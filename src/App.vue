4e8fb589dee3dfbea9d1eb05fdc59228916b6a7b
Codechad122 <footballers737@gmail.com>
2025-03-09 19:32:18 -0500

    set up more boiler plate routes and stub out basic contact form

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Home from './components/Home.vue'
 import Navbar from './components/Navbar.vue'
 import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Products from './components/Products.vue'
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import Products from './components/Products.vue'
 
 const routes = [
     {path: '/', component: Home},
-    {path: "/about", component: About}
+    {path: "/about", component: About},
+    {path: "/contact", component: Contact},
+    {path: "/products", component: Products}
 ]
 
 const router = createRouter({
