88db317a1b5046dd5a99d35e20cd12b9db861b92
Codechad122 <footballers737@gmail.com>
2025-03-11 13:18:22 -0500

    set up deploy pipeline to s3

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,6 +1,13 @@
 <script setup>
+import Navbar from './Navbar'
+import 
 </script>
 
 <template>
-    
+    <div class="h2">
+        <h2 class="text-2xl">Our Products</h2>
+        <div v-for="n in 9">
+
+        </div>
+    </div>
 </template>
