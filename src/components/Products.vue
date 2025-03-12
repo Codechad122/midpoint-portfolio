f2311617126a2800d18877083ab7b11e8b0f596e
Codechad122 <footballers737@gmail.com>
2025-03-11 22:48:16 -0500

    make more products

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,7 +2,17 @@
 import Product from './Product.vue'
 import Navbar from './Navbar.vue'
 const data = [
-    {title: "Product 1", info: "Product info", button: "Click me"}
+    {title: "Product 1", info: "Product info", button: "Click me"},
+    {title: "Product 2", info: "Product info", button: "Click me"},
+    {title: "Product 3", info: "Product info", button: "Click me"},
+    {title: "Product 4", info: "Product info", button: "Click me"},
+    {title: "Product 5", info: "Product info", button: "Click me"},
+    {title: "Product 6", info: "Product info", button: "Click me"},
+    {title: "Product 7", info: "Product info", button: "Click me"},
+    {title: "Product 8", info: "Product info", button: "Click me"},
+    {title: "Product 9", info: "Product info", button: "Click me"},
+    {title: "Product 10", info: "Product info", button: "Click me"},
+    {title: "Product 11", info: "Product info", button: "Click me"}
     ]
 </script>
 
@@ -14,7 +24,7 @@ const data = [
         <div class="h2">
             <h2 class="text-2xl">Our Products</h2>
             <div v-for="n in data" :key="n">
-                <Product :title="n.title" :info="n.info" :button="n.button"/>
+                <Product :title="n.title" :info="n.info"/>
 
             </div>
         </div>
